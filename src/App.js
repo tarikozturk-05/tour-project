a27a3c5ec5c2704c04ade2fd1d2595cba7baf4a1
Tarik <a.tarik2005@gmail.com>
2022-10-10 14:17:43 +0300

    pushlamayı unuttum :(

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
-
+import Navbar from './components/navbar/Navbar'
+import Header from './components/header/Header';
+import Card from './components/cards/Card'
+// import './helper/data'
 function App() {
   return (
     <div className="App">
-     
+       
+     <Navbar />
+     <Header />
+    <Card />
+
     </div>
   );
 }
